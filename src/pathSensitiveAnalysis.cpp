247b3fd4401bb71a5cad31a4bba44d48f972409a
Long Gong <lgong30@gatech.edu>
2018-04-17 10:49:35 -0400

    add RoI search

--- src/pathSensitiveAnalysis.cpp
+++ src/pathSensitiveAnalysis.cpp
@@ -43,6 +43,10 @@
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/SCCP.h"
 
+#include "llvm/IR/CFG.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/Analysis/CFG.h"//
 
 #include <set>
 #include <unordered_map>
@@ -63,30 +67,150 @@ namespace {
         return vStr;
     }
 
-    bool isConstant(SCCPSolver& Solver, Value *V){
-        Constant *Const = nullptr;
-        if (V->getType()->isStructTy()) {
-            std::vector<LatticeVal> IVs = Solver.getStructLatticeValueFor(V);
-            if (any_of(IVs, [](const LatticeVal &LV) { return LV.isOverdefined(); }))
-                return false;
-            std::vector<Constant *> ConstVals;
-            auto *ST = dyn_cast<StructType>(V->getType());
-            for (unsigned i = 0, e = ST->getNumElements(); i != e; ++i) {
-                LatticeVal V = IVs[i];
-                ConstVals.push_back(V.isConstant()
-                                    ? V.getConstant()
-                                    : UndefValue::get(ST->getElementType(i)));
-            }
-            Const = ConstantStruct::get(ST, ConstVals);
-        } else {
-            LatticeVal IV = Solver.getLatticeValueFor(V);
-            if (IV.isOverdefined())
-                return false;
-            Const = IV.isConstant() ? IV.getConstant() : UndefValue::get(V->getType());
+    std::unordered_map<BasicBlock*, std::set<BasicBlock*> > GetReachableNodes(Function &F){
+        std::unordered_map<BasicBlock*, std::set<BasicBlock*> > TransitiveClosureBB;
+        for (BasicBlock &BB : F) {
+            auto *TI = BB.getTerminator();
+            TransitiveClosureBB[&BB] = std::set<BasicBlock*>();
+            for (unsigned int i = 0;i < TI->getNumSuccessors();++ i)
+                TransitiveClosureBB[&BB].insert(TI->getSuccessor(i));
         }
-        return true;
+
+        for (BasicBlock &BBk : F) {
+            for (BasicBlock &BBi : F) {
+                for (BasicBlock &BBj : F) {
+                    if (TransitiveClosureBB[&BBi].count(&BBk) && TransitiveClosureBB[&BBk].count(&BBj)){
+                        // BBi -> BBk && BBk -> BBj
+                        TransitiveClosureBB[&BBi].insert(&BBj);
+                    }
+                }
+            }
+        }
+
+        return TransitiveClosureBB;
     }
 
+    void GetRoISmall(PHINode *PHI,
+                const std::set<Instruction*>& InfluencedNodes,
+                const std::unordered_map<BasicBlock*, std::set<BasicBlock*> >& TCBB,
+                std::vector<double>& BestFitness,
+                std::vector<std::set<Instruction*> >& RoI){
+
+        std::set<Instruction*> MyRoI;
+        BasicBlock* BBPHI = PHI->getParent();
+
+        for (Instruction* U_INFL: InfluencedNodes){
+            for(BasicBlock* REACH_BB_PHI: TCBB.at(BBPHI)){
+                if (TCBB.at(REACH_BB_PHI).count(U_INFL->getParent()))
+                {
+                    if (MyRoI.count(&REACH_BB_PHI->front()))
+                    {
+                        Instruction* InstPtr = &REACH_BB_PHI->front();
+                        while (!InstPtr)
+                            MyRoI.insert(InstPtr);
+
+                        if (InfluencedNodes.size() * 1.0 / MyRoI.size() <= BestFitness.back()) {
+                            return;
+                        }
+                    }
+                }
+            }
+            BasicBlock *UBB = U_INFL->getParent();
+            if (UBB == BBPHI || TCBB.at(BBPHI).count(UBB)){
+                Instruction* InstPtr = PHI->getNextNode();
+                do{
+                    if (!InstPtr) {
+                        MyRoI.insert(InstPtr);
+                        if (InfluencedNodes.size() * 1.0 / MyRoI.size() <= BestFitness.back())
+                            return;
+                    }
+                } while (!InstPtr || InstPtr != U_INFL);
+            }
+        }
+        double MyFitness = InfluencedNodes.size() * 1.0 / MyRoI.size();
+        assert(BestFitness.size() == 2 && "Sorry! This implementation only handles two best guys!");
+        if (MyFitness > BestFitness.front()) {
+            BestFitness.front() = MyFitness;
+            RoI.front() = MyRoI;
+        } else {
+            BestFitness.back() = MyFitness;
+            RoI.back() = MyRoI;
+        }
+    }
+    void GetRoILarge(PHINode *PHI,
+                     const std::set<Instruction*>& InfluencedNodes,
+                     Function& F,
+                     const DominatorTree *DT,
+                     const LoopInfo *LI,
+                     std::vector<double>& BestFitness,
+                     std::vector<std::set<Instruction*> >& RoI){
+
+        std::set<Instruction*> MyRoI;
+        BasicBlock* BBPHI = PHI->getParent();
+
+        for (Instruction* U_INFL: InfluencedNodes){
+            for (BasicBlock& BB: F){
+                BasicBlock *B = &BB;
+                if (isPotentiallyReachable(BBPHI, B, DT, LI) &&
+                        isPotentiallyReachable(B, U_INFL->getParent(), DT, LI))
+                {
+                    if (MyRoI.count(&B->front()))
+                    {
+                        Instruction* InstPtr = &B->front();
+                        while (!InstPtr)
+                            MyRoI.insert(InstPtr);
+
+                        if (InfluencedNodes.size() * 1.0 / MyRoI.size() <= BestFitness.back()) {
+                            return;
+                        }
+                    }
+                }
+            }
+            BasicBlock *UBB = U_INFL->getParent();
+            if (UBB == BBPHI || isPotentiallyReachable(BBPHI, UBB, DT, LI)){
+                Instruction* InstPtr = PHI->getNextNode();
+                do{
+                    if (!InstPtr) {
+                        MyRoI.insert(InstPtr);
+                        if (InfluencedNodes.size() * 1.0 / MyRoI.size() <= BestFitness.back())
+                            return;
+                    }
+                } while (!InstPtr || InstPtr != U_INFL);
+            }
+        }
+        double MyFitness = InfluencedNodes.size() * 1.0 / MyRoI.size();
+        assert(BestFitness.size() == 2 && "Sorry! This implementation only handles two best guys!");
+        if (MyFitness > BestFitness.front()) {
+            BestFitness.front() = MyFitness;
+            RoI.front() = MyRoI;
+        } else {
+            BestFitness.back() = MyFitness;
+            RoI.back() = MyRoI;
+        }
+    }
+//    bool isConstant(SCCPSolver& Solver, Value *V){
+//        Constant *Const = nullptr;
+//        if (V->getType()->isStructTy()) {
+//            std::vector<LatticeVal> IVs = Solver.getStructLatticeValueFor(V);
+//            if (any_of(IVs, [](const LatticeVal &LV) { return LV.isOverdefined(); }))
+//                return false;
+//            std::vector<Constant *> ConstVals;
+//            auto *ST = dyn_cast<StructType>(V->getType());
+//            for (unsigned i = 0, e = ST->getNumElements(); i != e; ++i) {
+//                LatticeVal V = IVs[i];
+//                ConstVals.push_back(V.isConstant()
+//                                    ? V.getConstant()
+//                                    : UndefValue::get(ST->getElementType(i)));
+//            }
+//            Const = ConstantStruct::get(ST, ConstVals);
+//        } else {
+//            LatticeVal IV = Solver.getLatticeValueFor(V);
+//            if (IV.isOverdefined())
+//                return false;
+//            Const = IV.isConstant() ? IV.getConstant() : UndefValue::get(V->getType());
+//        }
+//        return true;
+//    }
 
     struct PathSensitiveAnalysis : FunctionPass {
         static char ID;
@@ -99,6 +223,15 @@ namespace {
             const DataLayout &DL = F.getParent()->getDataLayout();
             TargetLibraryInfo *TLI =
                     &getAnalysis<TargetLibraryInfoWrapperPass>().getTLI();
+
+            const DominatorTree *DT =
+                    &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+            const LoopInfo *LI =
+                    &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+
+            std::unordered_map<PHINode*, std::set<Instruction*> > InfluencedNodes;
+            std::vector<std::set<Instruction*> > RoI(2);
+            std::vector<double> BestFitness(2, 0);
 
             SCCPSolver Solver(DL, TLI);
 
@@ -117,6 +250,12 @@ namespace {
                 ResolvedUndefs = Solver.ResolvedUndefsIn(F);
             }
 
+            std::unordered_map<BasicBlock*, std::set<BasicBlock *> > TCBB;
+            if (F.size() <= 64){
+                TCBB = GetReachableNodes(F);
+            }
+
+            double f1 = 0, f2 = 0;
             for (BasicBlock &BB : F) {
                 if (!Solver.isBlockExecutable(&BB)) continue;
                 // Iterate over all of the instructions in a function, replacing them with
@@ -127,20 +266,36 @@ namespace {
                     if (Inst->getType()->isVoidTy() || isa<TerminatorInst>(Inst))
                         continue;
                     if (auto* PHI = dyn_cast<PHINode>(Inst)){
+
                         if (Solver.isDestructiveMerge(PHI)){
                             errs() << toString(PHI)
                                    << " is a destructive merge.\n"
                                    << "its region of influence is ";
-                            for (auto UI = PHI->use_begin(), UE = PHI->use_end();UI != UE;){
-                                Use *U = &*UI++;
-                                errs() << toString(U) << "\n";
+                            for (auto UI = PHI->user_begin(), UE = PHI->user_end();UI != UE;){
+                                User *U = *UI++;
+                                if (Instruction *I = dyn_cast<Instruction>(U)) {
+                                    if (!InfluencedNodes.count(PHI)) {
+                                        InfluencedNodes[PHI] = std::set<Instruction*>();
+                                        // RoI[PHI] = std::set<Instruction*>();
+                                        // RoISize[PHI] = 0;
+                                    }
+                                    InfluencedNodes[PHI].insert(I);
+                                    errs() << toString(I) << "\n";
+                                }
                             }
                             errs() << "--------------------------------------------\n";
-                        }
 
+                            if (F.size() <= 64) GetRoISmall(PHI, InfluencedNodes.at(PHI), TCBB, BestFitness, RoI);
+                            else GetRoILarge(
+                                    PHI, InfluencedNodes.at(PHI), F, DT, LI,
+                                    BestFitness, RoI
+                                    );
+                        }
                     }
                 }
             }
+
+
 
 
             return Change;
@@ -149,6 +304,9 @@ namespace {
         void getAnalysisUsage(AnalysisUsage &AU) const override {
             AU.setPreservesCFG();
             AU.addRequired<TargetLibraryInfoWrapperPass>();
+            AU.addRequired<DominatorTreeWrapperPass>();
+            AU.addRequired<LoopInfoWrapperPass>();
+
         }
 
         void print(raw_ostream &os, const Module *) const override {}
--- tests/nonssa_cprop.ll
+++ tests/nonssa_cprop.ll
@@ -1,4 +1,4 @@
-; ModuleID = 'nonssa_cprop.ll'
+; ModuleID = 'cprop.cpp'
 source_filename = "cprop.cpp"
 target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
 target triple = "x86_64-unknown-linux-gnu"
@@ -6,75 +6,105 @@ target triple = "x86_64-unknown-linux-gnu"
 ; Function Attrs: noinline nounwind uwtable
 define i32 @_Z3fooii(i32 %a, i32 %b) #0 {
 entry:
-  %cmp = icmp sgt i32 %a, 10
+  %a.addr = alloca i32, align 4
+  %b.addr = alloca i32, align 4
+  %x = alloca i32, align 4
+  %y = alloca i32, align 4
+  store i32 %a, i32* %a.addr, align 4
+  store i32 %b, i32* %b.addr, align 4
+  store i32 2, i32* %y, align 4
+  %0 = load i32, i32* %a.addr, align 4
+  %cmp = icmp sgt i32 %0, 10
   br i1 %cmp, label %if.then, label %if.else
 
 if.then:                                          ; preds = %entry
+  store i32 1, i32* %x, align 4
   br label %if.end8
 
 if.else:                                          ; preds = %entry
-  %cmp1 = icmp sgt i32 %a, 5
+  %1 = load i32, i32* %a.addr, align 4
+  %cmp1 = icmp sgt i32 %1, 5
   br i1 %cmp1, label %if.then2, label %if.else3
 
 if.then2:                                         ; preds = %if.else
+  store i32 3, i32* %x, align 4
   br label %if.end7
 
 if.else3:                                         ; preds = %if.else
-  %cmp4 = icmp sgt i32 %a, 0
+  %2 = load i32, i32* %a.addr, align 4
+  %cmp4 = icmp sgt i32 %2, 0
   br i1 %cmp4, label %if.then5, label %if.else6
 
 if.then5:                                         ; preds = %if.else3
+  store i32 100, i32* %x, align 4
   br label %if.end
 
 if.else6:                                         ; preds = %if.else3
+  %3 = load i32, i32* %a.addr, align 4
+  store i32 %3, i32* %x, align 4
   br label %if.end
 
 if.end:                                           ; preds = %if.else6, %if.then5
-  %x.0 = phi i32 [ 100, %if.then5 ], [ %a, %if.else6 ]
   br label %if.end7
 
 if.end7:                                          ; preds = %if.end, %if.then2
-  %x.1 = phi i32 [ 3, %if.then2 ], [ %x.0, %if.end ]
   br label %if.end8
 
 if.end8:                                          ; preds = %if.end7, %if.then
-  %x.2 = phi i32 [ 1, %if.then ], [ %x.1, %if.end7 ]
   br label %D
 
 D:                                                ; preds = %if.end8
-  %cmp9 = icmp eq i32 %x.2, 8
+  %4 = load i32, i32* %x, align 4
+  %cmp9 = icmp eq i32 %4, 8
   br i1 %cmp9, label %if.then10, label %if.else11
 
 if.then10:                                        ; preds = %D
+  %5 = load i32, i32* %a.addr, align 4
+  store i32 %5, i32* %y, align 4
   br label %if.end16
 
 if.else11:                                        ; preds = %D
-  %cmp12 = icmp eq i32 %x.2, 9
+  %6 = load i32, i32* %x, align 4
+  %cmp12 = icmp eq i32 %6, 9
   br i1 %cmp12, label %if.then13, label %if.else14
 
 if.then13:                                        ; preds = %if.else11
+  %7 = load i32, i32* %b.addr, align 4
+  store i32 %7, i32* %y, align 4
   br label %if.end15
 
 if.else14:                                        ; preds = %if.else11
-  %add = add nsw i32 %a, %b
+  %8 = load i32, i32* %a.addr, align 4
+  %9 = load i32, i32* %b.addr, align 4
+  %add = add nsw i32 %8, %9
+  store i32 %add, i32* %y, align 4
   br label %if.end15
 
 if.end15:                                         ; preds = %if.else14, %if.then13
   br label %if.end16
 
 if.end16:                                         ; preds = %if.end15, %if.then10
-  %cmp17 = icmp sgt i32 %x.2, %a
+  %10 = load i32, i32* %x, align 4
+  store i32 %10, i32* %y, align 4
+  %11 = load i32, i32* %y, align 4
+  %12 = load i32, i32* %a.addr, align 4
+  %cmp17 = icmp sgt i32 %11, %12
   br i1 %cmp17, label %if.then18, label %if.else19
 
 if.then18:                                        ; preds = %if.end16
+  %13 = load i32, i32* %b.addr, align 4
+  store i32 %13, i32* %y, align 4
   br label %if.end20
 
 if.else19:                                        ; preds = %if.end16
+  %14 = load i32, i32* %a.addr, align 4
+  store i32 %14, i32* %y, align 4
   br label %if.end20
 
 if.end20:                                         ; preds = %if.else19, %if.then18
-  %y.0 = phi i32 [ %b, %if.then18 ], [ %a, %if.else19 ]
-  %add21 = add nsw i32 %x.2, %y.0
+  %15 = load i32, i32* %x, align 4
+  %16 = load i32, i32* %y, align 4
+  %add21 = add nsw i32 %15, %16
   ret i32 %add21
 }
 
