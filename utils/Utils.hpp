4030b692b06522e7d4babbd4e54ffa05aa78a05d
Saber Dragon <saber_dragon@outlook.com>
2018-04-20 16:51:56 -0400

    add CFG Split

--- utils/Utils.hpp
+++ utils/Utils.hpp
@@ -1,6 +1,16 @@
 #ifndef PATHSENSITIVEANALYSIS_UTILS_HPP
 #define PATHSENSITIVEANALYSIS_UTILS_HPP
 
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseSet.h"
+#include "llvm/ADT/PointerIntPair.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/ADT/GraphTraits.h"  // for GraphTraits
+
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/User.h"
 #include "llvm/IR/Value.h"
@@ -11,6 +21,8 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include <string>
+#include <bits/unordered_map.h>
+
 using namespace llvm;
 
 namespace saber{
@@ -51,5 +63,22 @@ namespace saber{
             errs() << "\n" << toString(Inst) << "\n";
         }
     }
+
+    template <typename T>
+    T  get(const DenseMap<T, T>& TMap, const T& Key){
+        if (TMap.count(Key)) return TMap.lookup(Key);
+        return Key;
+    }
+
+    Value *get(const ValueToValueMapTy& VMap, Value* OldValue){
+        if (VMap.count(OldValue)) return VMap.lookup(OldValue);
+        return OldValue;
+    }
+
+//    template <typename T>
+//    T get(const std::unordered_map<T, T>& TMap, const T& Key){
+//        if (TMap.count(Key)) return TMap.at(Key);
+//        else return Key;
+//    }
 }
 #endif //PATHSENSITIVEANALYSIS_UTILS_HPP
